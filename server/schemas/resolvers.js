fc9e3e36cc822612b4b2b2717919ef2977e6afa8
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-05-07 18:49:27 -0700

    fixed missing query in resolvers that kept my server from functioning

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,8 @@
 //import models
-const { User, Book } = require('../models');
+const { User} = require('../models');
 
 const resolvers = {
-
+Query:{
     users: async () => {
         return await User.find().select('-__v -password').populate('savedBooks');
     },
@@ -10,6 +10,7 @@ const resolvers = {
     user: async (parent, { username }) => {
         return await User.findOne({ username }).select('-__v -password').populate('savedBooks');
     }
+}
 };
 
 module.exports = resolvers;
